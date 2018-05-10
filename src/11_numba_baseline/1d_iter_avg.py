Fixes to 1D iterative averaging

src/11_numba_baseline/1d_iter_avg.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 import sys
+import time
 import numpy as np
+import matplotlib
+matplotlib.use('agg')
 
 def kernel(nxt, curr, N):
     for i in range(1, N + 1):
@@ -13,17 +16,23 @@ def driver(niters, seed):
     nxt[0] = seed[0]
     nxt[-1] = seed[-1]
 
+    start_time = time.time()
     for iter in range(niters):
         kernel(nxt, curr, len(curr) - 2)
 
         tmp = nxt
         nxt = curr
         curr = tmp
+    elapsed_time = time.time() - start_time
+
+    print('Elapsed time for N=' + str(len(seed) - 2) + ', # iters=' +
+            str(niters) + ' is ' + str(elapsed_time) + ' s')
+    print(str(float(niters) / elapsed_time) + ' iters / s')
 
     return curr
 
 if len(sys.argv) != 3:
-    sys.stderr('usage: python 1d_iter_avg.py <N> <niters>\n')
+    sys.stderr.write('usage: python 1d_iter_avg.py <N> <niters>\n')
     sys.exit(1)
 
 N = int(sys.argv[1])
@@ -33,6 +42,13 @@ seed = np.zeros(N + 2)
 seed[-1] = 1.0
 result = driver(niters, seed)
 
+# Save to image
 from matplotlib import pyplot as plt
-plt.imshow(result, interpolation='nearest')
-plt.show()
+ny = int(len(result) / 10)
+if ny < 1:
+    ny = 1
+img = np.zeros((ny, len(result)))
+for i in range(img.shape[0]):
+    img[i, :] = result
+plt.imshow(img, interpolation='nearest')
+plt.savefig('img.png')
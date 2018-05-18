7700a7e19eaaf6e09d4bf3d866d754aa68b98d2e
Max Grossman <jmaxg3@gmail.com>
2018-05-18 18:55:54 -0500

    Revise NN example to be simpler and better match flow of linear regression example

--- src/15_tf_nn/neural_network.py
+++ src/15_tf_nn/neural_network.py
@@ -15,7 +15,10 @@ Project: https://github.com/aymericdamien/TensorFlow-Examples/
 """
 
 from __future__ import print_function
+import os
 
+# User your username on DAVINCI to create a directory for storing the MNIST
+# dataset
 user = os.environ['USER']
 
 # Import MNIST data
@@ -26,7 +29,7 @@ import tensorflow as tf
 
 # Parameters
 learning_rate = 0.1
-num_steps = 1000
+num_epochs = 20
 batch_size = 128
 display_step = 100
 
@@ -37,69 +40,90 @@ num_input = 784 # MNIST data input (img shape: 28*28)
 num_classes = 10 # MNIST total classes (0-9 digits)
 
 
-# Define the neural network
-def neural_net(x_dict):
-    # TF Estimator input is a dict, in case of multiple inputs
-    x = x_dict['images']
+# Define the neural network to take an input of 'batch_size' images, each with
+# 'num_input' pixels with 2 hidden layers, each with 256 neurons.
+def neural_net():
+    x = tf.placeholder('float', shape = (batch_size, num_input), name = 'images')
     # Hidden fully connected layer with 256 neurons
     layer_1 = tf.layers.dense(x, n_hidden_1)
     # Hidden fully connected layer with 256 neurons
     layer_2 = tf.layers.dense(layer_1, n_hidden_2)
     # Output fully connected layer with a neuron for each class
     out_layer = tf.layers.dense(layer_2, num_classes)
-    return out_layer
+    return x, out_layer
 
 
-# Define the model function (following TF Estimator Template)
-def model_fn(features, labels, mode):
-    # Build the neural network
-    logits = neural_net(features)
+# Compute the error between the expected labels and the computed labels across
+# the whole training dataset. Error is defined by the loss_op operation.
+def compute_err(loss_op, image_placeholder, label_placeholder, images, labels):
+    err = 0.0
+    n_batches = 0
+    start_batch = 0
+    while start_batch + batch_size < mnist.train.images.shape[0]:
+        err += sess.run(loss_op,
+                feed_dict = {image_placeholder: images[start_batch:start_batch + batch_size, :],
+                             label_placeholder: labels[start_batch:start_batch + batch_size]})
+        start_batch += batch_size
+        n_batches += 1
+    return err / float(n_batches)
 
-    # Predictions
-    pred_classes = tf.argmax(logits, axis=1)
-    pred_probas = tf.nn.softmax(logits)
+# Construct the TF graph model, returning the input placeholder as 'images' and
+# the output as 'logits'.
+images, logits = neural_net()
 
-    # If prediction mode, early return
-    if mode == tf.estimator.ModeKeys.PREDICT:
-        return tf.estimator.EstimatorSpec(mode, predictions=pred_classes)
+# Create a placeholder to store the expected values for each batch, for error
+# calculation by the optimizer.
+labels = tf.placeholder('uint8', shape = (batch_size), name = 'labels')
 
-        # Define loss and optimizer
-    loss_op = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
-        logits=logits, labels=tf.cast(labels, dtype=tf.int32)))
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
-    train_op = optimizer.minimize(loss_op,
-                                  global_step=tf.train.get_global_step())
+# Measure the loss between the computed logits and the expected labels
+loss_op = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
+    logits=logits, labels=tf.cast(labels, dtype=tf.int32)))
 
-    # Evaluate the accuracy of the model
-    acc_op = tf.metrics.accuracy(labels=labels, predictions=pred_classes)
+# Configure an optimizer with the given learning rate
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
 
-    # TF Estimators requires to return a EstimatorSpec, that specify
-    # the different ops for training, evaluating, ...
-    estim_specs = tf.estimator.EstimatorSpec(
-        mode=mode,
-        predictions=pred_classes,
-        loss=loss_op,
-        train_op=train_op,
-        eval_metric_ops={'accuracy': acc_op})
+# Ask the optimizer to minimize the model loss defined above
+train_op = optimizer.minimize(loss_op,
+                              global_step=tf.train.get_global_step())
 
-    return estim_specs
+init = tf.global_variables_initializer()
 
-# Build the Estimator
-model = tf.estimator.Estimator(model_fn)
+with tf.Session() as sess:
+    # Initialize all variables
+    sess.run(init)
 
-# Define the input function for training
-input_fn = tf.estimator.inputs.numpy_input_fn(
-    x={'images': mnist.train.images}, y=mnist.train.labels,
-    batch_size=batch_size, num_epochs=None, shuffle=True)
-# Train the Model
-model.train(input_fn, steps=num_steps)
+    # Iterate for the specified number of epochs
+    for epoch in range(num_epochs):
 
-# Evaluate the Model
-# Define the input function for evaluating
-input_fn = tf.estimator.inputs.numpy_input_fn(
-    x={'images': mnist.test.images}, y=mnist.test.labels,
-    batch_size=batch_size, shuffle=False)
-# Use the Estimator 'evaluate' method
-e = model.evaluate(input_fn)
+        # Batch the inputs into batches of length 'batch_size' and pass them
+        # through the optimizer batch-by-batch.
+        start_batch = 0
+        while start_batch + batch_size < mnist.train.images.shape[0]:
+            sess.run(train_op, feed_dict = {images: mnist.train.images[start_batch:start_batch + batch_size, :],
+                                            labels: mnist.train.labels[start_batch:start_batch + batch_size]})
+            start_batch += batch_size
 
-print("Testing Accuracy:", e['accuracy'])
+        # Compute and print the error of the current model on the training data
+        err = compute_err(loss_op, images, labels, mnist.train.images,
+                mnist.train.labels)
+        print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(err))
+
+    # Evaluate the trained model on MNIST testing data
+    pred_classes = tf.argmax(logits, axis = 1)
+    computed_labels = []
+    start_batch = 0
+    while start_batch + batch_size < mnist.test.images.shape[0]:
+        predictions = sess.run(pred_classes,
+                feed_dict = {images: mnist.test.images[start_batch:start_batch + batch_size, :]})
+        for p in predictions:
+            computed_labels.append(p)
+        start_batch += batch_size
+
+    # Count how many of the labels computed by our model match the expected
+    # labels
+    count_same = 0
+    for i in range(len(computed_labels)):
+        if computed_labels[i] == mnist.test.labels[i]:
+            count_same += 1
+    print(count_same, '/', len(computed_labels), '=',
+            100.0 * float(count_same) / float(len(computed_labels)), '%')
